abec6808069d340601bb6321e22b0d8c2a22c587
CursedDomino <dillon.daughrity@outlook.com>
2025-09-29 20:30:49 -0500

    General Styling Updates/Functionality Updates

--- main.js
+++ main.js
@@ -7,21 +7,73 @@ const customProgressBar = document.getElementById("custom-progress-bar");
 const baseURL = "/songs";
 
 let currentTrack = 0;
+let savedTime = 0;
+let savedPlayback = 1;
 
 const songs = [
-  { file: "Aidan.mp3", albulmArt: "Aiden.jpg" },
-  { file: "autumn_sun.mp3", albulmArt: "BestPart.jpg" },
+  { file: "Aidan.mp3", albumArt: "Aidan.jpg" },
+  { file: "autumn_sun.mp3", albumArt: "autumn_sun.jpg" },
+  { file: "best_part_of_me.mp3", albumArt: "BestPart.jpg" },
+  { file: "Better Days - LAKEY INSPIRED.mp3", albumArt: "Better Days.jpg" },
+  { file: "i_cant_make_you_love_me_cover.mp3", albumArt: "i_cant_make_you_love_me_cover.jpeg" },
+  { file: "just_relax.mp3", albumArt: "justRelax_img.jpeg" },
+  { file: "paranormal-is-real-leonell-cassio.mp3", albumArt: "paranormal_real_500.jpg" },
+  { file: "Polarity.mp3", albumArt: "Polarity.jpg" },
 ];
 
 playButton.addEventListener("click", () => {
   audioPlayer.src = `${baseURL}/${songs[currentTrack].file}`;
   audioPlayer.load();
+  audioPlayer.currentTime = savedTime;
+  audioPlayer.playbackRate = savedPlayback;
+  playButton.classList.toggle("hidden");
+  pauseButton.classList.toggle("hidden");
   audioPlayer.play();
 });
 
 pauseButton.addEventListener("click", () => {
   console.log("pause");
+  savedTime = audioPlayer.currentTime;
+  savedPlayback = audioPlayer.playbackRate;
+  playButton.classList.toggle("hidden");
+  pauseButton.classList.toggle("hidden");
   audioPlayer.pause();
+});
+
+prevButton.addEventListener("click", () => {
+  if (!pauseButton.classList.contains("hidden")) {
+    audioPlayer.pause();
+    playButton.classList.toggle("hidden");
+    pauseButton.classList.toggle("hidden");
+  }
+  console.log("prev");
+  savedTime = 0;
+  savedPlayback = 1;
+  audioPlayer.currentTime = savedTime;
+  audioPlayer.playbackRate = savedPlayback;
+  currentTrack > 0 ? (currentTrack -= 1) : (currentTrack = 7);
+});
+
+nextButton.addEventListener("click", () => {
+  if (!pauseButton.classList.contains("hidden")) {
+    audioPlayer.pause();
+    playButton.classList.toggle("hidden");
+    pauseButton.classList.toggle("hidden");
+  }
+  console.log("next");
+  savedTime = 0;
+  savedPlayback = 1;
+  audioPlayer.currentTime = savedTime;
+  audioPlayer.playbackRate = savedPlayback;
+  currentTrack < 7 ? (currentTrack += 1) : (currentTrack = 0);
+});
+
+skipButton.addEventListener("click", () => {
+  audioPlayer.currentTime += 5;
+});
+
+rewindButton.addEventListener("click", () => {
+  audioPlayer.currentTime -= 5;
 });
 
 audioPlayer.addEventListener("timeupdate", () => {
@@ -36,7 +88,7 @@ audioPlayer.addEventListener("timeupdate", () => {
 });
 
 document.addEventListener("keydown", (event) => {
-  switch (event.key.toLowerCase()) {   
+  switch (event.key.toLowerCase()) {
     case " ":
       event.preventDefault();
       audioPlayer.paused ? audioPlayer.play() : audioPlayer.pause();
